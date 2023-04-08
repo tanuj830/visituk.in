97a68353c1ab1d73dacb7c83f404d56ef94f8aa5
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2023-04-08 21:22:25 +0530

    whatsapp-and-call-floating-added

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,12 +12,13 @@ import { RxCross1 } from "react-icons/rx";
 import { FaRegKeyboard } from "react-icons/fa";
 import { TbBrandBlogger } from "react-icons/tb";
 import axios from "axios";
-import { MdModeOfTravel, MdOutlineLocalOffer } from "react-icons/md";
+import { MdCall, MdModeOfTravel, MdOutlineLocalOffer } from "react-icons/md";
+import { RiWhatsappFill } from "react-icons/ri";
 
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = React.useState(false);
-
+  const [showNumber,setShowNumber]=React.useState(false);
   const handleClick = () => {
     setShowMenu(!showMenu);
   };
@@ -80,7 +81,12 @@ const Navbar = () => {
             <hr />
           </div>
         </div>
-
+        <div className="fixed top-64 right-0  z-10 rounded-l-md drop-shadow-md bg-gray-100">
+          <ul className="p-1 font-bold text-lg">
+            <li className="flex m-2  items-center">{showNumber && <span className="mr-2">818-181-3626</span>} <RiWhatsappFill onMouseEnter={()=>setShowNumber(true)} onMouseLeave={()=>setShowNumber(false)} size={30} /></li>
+            <li className="flex m-2  items-center">{showNumber && <span className="mr-2">818-181-3626</span>}<MdCall onMouseEnter={()=>setShowNumber(true)} onMouseLeave={()=>setShowNumber(false)} size={30} /></li>
+          </ul>
+        </div>
       </div>
 
     </>
