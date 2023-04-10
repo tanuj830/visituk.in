fa36a7b162df84f147ced24e13bc87ba5ed63039
tanuj <[artravellers98@gmail.com]>
2023-04-10 20:43:04 +0530

    offerings section added...!

--- src/components/herosection/Hero1.jsx
+++ src/components/herosection/Hero1.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Hero1 = () => {
   return (
-    <div className='xl:container mt-32 px-[1rem] md:px-[3rem] overflow-hidden'>
+    <div className='xl:container mt-32 px-[1rem] md:px-[7rem] '>
     <div className='flex flex-col md:flex-row   items-center justify-between gap-6'>
         <div className=''>
             <h4 className='text-3xl font-semibold'>Find The Perfect Package</h4>
@@ -11,17 +11,30 @@ const Hero1 = () => {
         <Link className='uppercase  duration-300 ease-in-out rounded-full text-sm text-green-500 hover:text-green-700' href="/">Explore All packages</Link>
         </div>
             <div className=' hidden md:inline-block '>
-            <div className='grid grid-cols-1   gap-6'>
+            <div className='grid grid-cols-1  '>
             <img className='w-72 rounded-2xl h-[27vw] hover:shadow-xl hover:scale-110 duration-150' src="https://images.pexels.com/photos/15335740/pexels-photo-15335740.jpeg" alt="" />
             </div>
             </div>
-       <div className='overflow-hidden'>
-       <div className='md:grid   md:grid-cols-2 flex flex-row flex-nowrap overflow-scroll gap-6'>
+       <div className=''>
+        {/* for mobile */}
+       <div className='inline md:hidden'>
+       <div className=' flex flex-row flex-nowrap overflow-scroll gap-6'>
             <img className='w-72 rounded-2xl hover:shadow-xl hover:scale-110 duration-150 ease-in-out' src="https://images.pexels.com/photos/15017640/pexels-photo-15017640.jpeg" alt="" />
             <img className='w-72 rounded-2xl hover:shadow-xl hover:scale-110 duration-150' src="https://images.pexels.com/photos/11305767/pexels-photo-11305767.jpeg" alt="" />
             {/* <img className='w-72 rounded-2xl hover:shadow-xl hover:scale-110 duration-150' src="https://images.pexels.com/photos/13022659/pexels-photo-13022659.jpeg" alt="" /> */}
             <img className='w-72 rounded-2xl hover:shadow-xl hover:scale-110 duration-150' src="https://images.pexels.com/photos/12584382/pexels-photo-12584382.jpeg" alt="" />
             <img className='w-72 rounded-2xl hover:shadow-xl hover:scale-110 duration-150' src="https://images.pexels.com/photos/12584382/pexels-photo-12584382.jpeg" alt="" />
+        </div>
+        </div>
+        {/* for pc */}
+       <div className='hidden md:inline'>
+       <div className=' grid grid-cols-2  gap-6'>
+            <img className='w-72 rounded-2xl hover:shadow-xl hover:scale-110 duration-150 ease-in-out' src="https://images.pexels.com/photos/15017640/pexels-photo-15017640.jpeg" alt="" />
+            <img className='w-72 rounded-2xl hover:shadow-xl hover:scale-110 duration-150' src="https://images.pexels.com/photos/11305767/pexels-photo-11305767.jpeg" alt="" />
+            {/* <img className='w-72 rounded-2xl hover:shadow-xl hover:scale-110 duration-150' src="https://images.pexels.com/photos/13022659/pexels-photo-13022659.jpeg" alt="" /> */}
+            <img className='w-72 rounded-2xl hover:shadow-xl hover:scale-110 duration-150' src="https://images.pexels.com/photos/12584382/pexels-photo-12584382.jpeg" alt="" />
+            <img className='w-72 rounded-2xl hover:shadow-xl hover:scale-110 duration-150' src="https://images.pexels.com/photos/12584382/pexels-photo-12584382.jpeg" alt="" />
+        </div>
         </div>
        </div>
         </div>
