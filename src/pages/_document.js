f98d90bb667c8868fe86c931a8e898a021e83519
tanuj <[artravellers98@gmail.com]>
2023-04-11 15:59:21 +0530

    offerings section added...!

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head /><body>
         
-        <div className=''>
+        <div className='relative'>
         </div>
         <Main />
         <NextScript />
