a5c0d957e21b8a22fd79559f755acbf9bf88603f
tanuj <[artravellers98@gmail.com]>
2023-04-30 00:43:05 +0530

    animation added

--- src/components/herosection/Hero3.jsx
+++ src/components/herosection/Hero3.jsx
@@ -20,21 +20,42 @@ const Hero3 = () => {
         },
     ]
   return (
+    <>
+        <div className='hidden md:inline'>
         <div className='flex items-center justify-center h-[100vh] bg-no-repeat ' style={{background:"url(https://images.pexels.com/photos/235778/pexels-photo-235778.jpeg?auto=compress&cs=tinysrgb&w=2840)", backgroundSize:"100%, 100%", backgroundRepeat:"no-repeat"}}>
     
-         <div className='mx-[1rem] md:mx-[4rem]'>
-         <div className='grid grid-cols-1 md:grid-cols-4 gap-3 md:gap-10 '>
-          {
-            data.map(d=>(
-                <div className="tracking-widest shadow-lg backdrop-blur-lg  bg-slate-100 bg-opacity-10 md:p-10 p-4 rounded-lg md:rounded-2xl">
-                        <h2 className='border-l-8 px-2 border-green-600 text-xl font-bold text-slate-50 brightness-200'>{d.title}</h2>
-                        <h3 className='text-sm mt-3 text-slate-300 brightness-200'>{d.disp}</h3>
-                    </div>
-            ))
-           }
-          </div>
-         </div>
+    <div className='mx-[1rem] md:mx-[4rem]'>
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-3 md:gap-10 '>
+     {
+       data.map(d=>(
+           <div className="tracking-widest shadow-lg backdrop-blur-lg  bg-slate-100 bg-opacity-10 md:p-10 p-4 rounded-lg md:rounded-2xl">
+                   <h2 className='border-l-8 px-2 border-green-600 text-xl font-bold text-slate-50 brightness-200'>{d.title}</h2>
+                   <h3 className='text-sm mt-3 text-slate-300 brightness-200'>{d.disp}</h3>
+               </div>
+       ))
+      }
+     </div>
     </div>
+        </div>
+    </div>
+        <div className='inline md:hidden'>
+        <div className='flex items-center justify-center bg-contain bg-no-repeat py-10 ' style={{background:"url(https://images.pexels.com/photos/2469122/pexels-photo-2469122.jpeg?auto=compress&cs=tinysrgb&w=600)", backgroundSize:"100%, 100%", backgroundRepeat:"no-repeat"}}>
+    
+    <div className='mx-[1rem] md:mx-[4rem]'>
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-3 md:gap-10 '>
+     {
+       data.map(d=>(
+           <div className="tracking-widest shadow-lg backdrop-blur-lg  bg-slate-100 bg-opacity-10 md:p-10 p-4 rounded-lg md:rounded-2xl">
+                   <h2 className='border-l-8 px-2 border-green-600 text-xl font-bold text-slate-50 brightness-200'>{d.title}</h2>
+                   <h3 className='text-sm mt-3 text-slate-300 brightness-200'>{d.disp}</h3>
+               </div>
+       ))
+      }
+     </div>
+    </div>
+</div>
+        </div>
+    </>
   )
 }
 
